5b6618d0e8ef13bb77dfd41f2a1984888fc2b9f2
Tancd <caotan1008@gmail.com>
2023-07-12 19:06:46 +0900

    .

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
-import Routes from "../../routes/Routers";
 
 import Carts from "../UI/cart/Carts.jsx";
 import { useSelector } from "react-redux";
