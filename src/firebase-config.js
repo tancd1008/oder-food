9955085be7b20d33510050048675d3c9e1bca4b6
namdd1710 <donam17102002@gmail.com>
2023-07-21 09:56:48 +0700

    feat: connect new firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,14 +3,15 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOJr4sBSmfkym3hubrZOmSKlBXmdJKB2s",
-    authDomain: "oderfood-82fbe.firebaseapp.com",
-    projectId: "oderfood-82fbe",
-    storageBucket: "oderfood-82fbe.appspot.com",
-    messagingSenderId: "874195952578",
-    appId: "1:874195952578:web:8450ef17475206ddf12c5b"
-  };
-  const app = initializeApp(firebaseConfig);
-  const fireDb = getDatabase(app)
-  export const storage = getStorage(app);
-  export default fireDb
+  apiKey: "AIzaSyD9gn3JG5TezkMxG5CfAG-fL71qu-8iJpw",
+  authDomain: "oderfood2-143a1.firebaseapp.com",
+  projectId: "oderfood2-143a1",
+  storageBucket: "oderfood2-143a1.appspot.com",
+  messagingSenderId: "1016360975977",
+  appId: "1:1016360975977:web:f3caeab1b6864333a0ecf8",
+  measurementId: "G-2CPBWMXBR1",
+};
+const app = initializeApp(firebaseConfig);
+const fireDb = getDatabase(app);
+export const storage = getStorage(app);
+export default fireDb;
