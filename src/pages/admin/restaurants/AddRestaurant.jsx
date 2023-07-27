0ac7a2ccbe6c5e90d0effd27cca7321d914dfb5c
Tancd <caotan1008@gmail.com>
2023-07-27 10:30:10 +0900

    input time

--- src/pages/admin/restaurants/AddRestaurant.jsx
+++ src/pages/admin/restaurants/AddRestaurant.jsx
@@ -1,19 +1,47 @@
 import React, { useState } from "react";
 import { createRestaurant } from "../../../services/restaurents";
+import Select from "react-select";
+
+import "./../../../styles/time-picker.css"
+
 const initialState = {
   nameRestaurant: "",
   address:"",
   status: 0
 };
+// const options = [
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:30", label: "00:30" },
+//   { value: "01:00", label: "01:00" },
+//   { value: "01:30", label: "01:30" },
+//   { value: "02:00", label: "02:00" },
+//   { value: "02:30", label: "02:30" },
+//   { value: "03:00", label: "03:00" },
+//   { value: "03:30", label: "03:30" },
+//   { value: "04:00", label: "04:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+//   { value: "00:00", label: "00:00" },
+ 
+// ];
 const AddRestaurant = () => {
   const [state, setState] = useState(initialState);
+  const [selectedTime, setSelectedTime] = useState('12:00')
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setState({ ...state, [name]: value });
   };
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    createRestaurant(state)
+    // createRestaurant(state)
     console.log(state);
   };
 
@@ -74,6 +102,58 @@ const AddRestaurant = () => {
 
           />
         </div>{" "}
+        <div className="mb-3 row">
+        <div className="col row">
+            <div className="col col-md-auto p-2">
+
+            <label>Giờ mở cửa</label>
+            </div>
+            <div className="col">
+            <input
+            type="time"
+            id="time_start"
+            name="time_start"
+            className="form-control"
+            placeholder="Số điện thoại"
+            onChange={handleInputChange}
+
+          />
+          {/* <Select
+            name="time_start"
+            options={options}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={handleTimeSelect}
+          /> */}
+            </div>
+          </div>
+          <div className="col row">
+            <div className="col col-md-auto p-2">
+
+            <label>Giờ đóng cửa</label>
+            </div>
+            <div className="col">
+            <input
+            type="time"
+            id="time_end"
+            name="time_end"
+            className="form-control"
+            placeholder="Số điện thoại"
+            onChange={handleInputChange}
+
+          />
+          {/* <Select
+            name="time_end"
+            options={options}
+            className="basic-multi-select"
+            classNamePrefix="select"
+            onChange={handleTimeSelect}
+          /> */}
+            </div>
+          </div>
+        </div>
+       
+        
         <button type="submit" className="btn btn-primary">
           Thêm mới
         </button>
