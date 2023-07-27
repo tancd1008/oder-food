562c70ff223c4ad561e9707523ad51633857df20
Tancd <caotan1008@gmail.com>
2023-07-27 14:38:02 +0900

    update list category

--- src/pages/admin/categories/ListCategories.jsx
+++ src/pages/admin/categories/ListCategories.jsx
@@ -6,20 +6,31 @@ import ConfirmBox from "../../../components/ConfirmBox";
 import { deleteProduct, updateProduct } from "../../../services/products";
 import { Link } from "react-router-dom";
 import { getAllCategoriesInRestaurant } from "../../../services/category";
+import { getAllRestaurants } from "../../../services/restaurents";
 const ListCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
   const [showConfirm, setShowConfirm] = useState(false);
-  const user = JSON.parse(sessionStorage.getItem('user'));
+  const user = JSON.parse(sessionStorage.getItem("user"));
   useEffect(() => {
     const getCategory = async () => {
       try {
-        const listCategories = await getAllCategoriesInRestaurant(user.restaurantId);
-     setCategories(listCategories)
+        if (!user.restaurantId) {
+        } else {
+          const listCategories = await getAllCategoriesInRestaurant(
+            user.restaurantId
+          );
+          setCategories(listCategories);
+        }
       } catch (error) {
         console.error("Đã xảy ra lỗi khi lấy danh mục:", error);
       }
-     
     };
+    const fetchRestaurants = async () => {
+      const restaurantList = await getAllRestaurants();
+      setRestaurants(restaurantList);
+    };
+    fetchRestaurants();
     getCategory();
   }, []);
   const handleDelete = (id) => {
@@ -27,43 +38,49 @@ const ListCategories = () => {
     deleteProduct(id);
     setShowConfirm(false);
   };
-
   const handleCancel = () => {
     setShowConfirm(false);
   };
   const handleUpdateStatus = (product) => {
-    var newProduct = {...product}
-    if(product.status === 0) {
-       newProduct = {...product, status: 1}
-      console.log("1")
-    }else{
-      console.log("0")
-       newProduct = {...product, status: 0}
+    var newProduct = { ...product };
+    if (product.status === 0) {
+      newProduct = { ...product, status: 1 };
+      console.log("1");
+    } else {
+      console.log("0");
+      newProduct = { ...product, status: 0 };
     }
-    updateProduct(product.id,newProduct)
-    console.log(newProduct)
-  }
-  console.log(categories)
+    updateProduct(product.id, newProduct);
+    console.log(newProduct);
+  };
+  console.log(restaurants);
   return (
     <div>
       <div>
         <div className="row">
-
-      <h1 className="text-center">Danh sách danh mục</h1> 
+          <h1 className="text-center">Danh sách danh mục</h1>
         </div>
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
           <div className="">
-          <select name="" id="">
-            <option value="">Nhà hàng 1</option>
-          </select>
+            <select name="" id="">
+              {restaurants.map((restaurant, index) => (
+                <option value={restaurant.id} key={index}>
+                  {restaurant.nameRestaurant}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="">
-
-          <Link to="/admin/category/add" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i className="fas fa-download fa-sm text-white-50" /> Thêm danh mục</Link>
+            <Link
+              to="/admin/category/add"
+              className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+            >
+              <i className="fas fa-download fa-sm text-white-50" /> Thêm danh
+              mục
+            </Link>
           </div>
           <div></div>
         </div>
-
       </div>
       <table className="table table-hover text-nowrap">
         <thead>
@@ -83,11 +100,21 @@ const ListCategories = () => {
               <th>{index + 1}</th>
               <th>{category.name}</th>
               <th>
-                <img className="rounded mx-auto d-block w-25 h-25" src={category.imgSrc} alt=""/>
+                <img
+                  className="rounded mx-auto d-block w-25 h-25"
+                  src={category.imgSrc}
+                  alt=""
+                />
               </th>
               <th>{category.price}</th>
               <th>{category.desc}</th>
-              <th >{category.status === 0 ? <p className="text-success">Hoạt động</p> : <p className="text-danger">Ngừng bán</p>}</th>
+              <th>
+                {category.status === 0 ? (
+                  <p className="text-success">Hoạt động</p>
+                ) : (
+                  <p className="text-danger">Ngừng bán</p>
+                )}
+              </th>
               <th className="">
                 <button
                   className="btn btn-danger"
@@ -99,12 +126,21 @@ const ListCategories = () => {
                   show={showConfirm}
                   message="Bạn có chắc chắn muốn xóa bản ghi này không?"
                   onConfirm={() => handleDelete(category.id)}
-                  onCancel={()=>handleCancel()}
+                  onCancel={() => handleCancel()}
                 />
                 <Link to={`/admin/category/edit/${category.id}`}>
-                <button className="btn btn-warning ms-1">Sửa</button>
+                  <button className="btn btn-warning ms-1">Sửa</button>
                 </Link>
-                <button className={`${category.status === 0 ? "btn btn-secondary ms-1" : "btn btn-success ms-1"}`}  onClick={() => handleUpdateStatus(category)}>{category.status === 0 ? "Dừng" : "Bán"}</button>
+                <button
+                  className={`${
+                    category.status === 0
+                      ? "btn btn-secondary ms-1"
+                      : "btn btn-success ms-1"
+                  }`}
+                  onClick={() => handleUpdateStatus(category)}
+                >
+                  {category.status === 0 ? "Dừng" : "Bán"}
+                </button>
               </th>
             </tr>
           ))}
